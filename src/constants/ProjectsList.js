ab7539af56b2350460858dfb66a73e2c529094ed
khalidAtGit <khalid.work97@gmail.com>
2024-11-14 23:19:26 +0530

    new update

---
+++ src/constants/ProjectsList.js
@@ -0,0 +1,91 @@
+const projects = [
+    {
+        id: 1,
+        project_image: '........',
+        project_title: '..........',
+        live_url: '...........',
+        source_url: '............',
+        description: '..........',
+        stack: [
+            'React',
+            'ContextAPI',
+            'SASS',
+            'Firebase',
+            'React Router'
+        ]
+    },
+    {
+        id: 2,
+        project_image: '...........',
+        project_title: '...........',
+        live_url: '........................',
+        source_url: '.......................',
+        description: '.................',
+        stack: [
+            'React',
+            'ContextAPI',
+            'SASS',
+            'MaterialUI',
+            'Firebase',
+            'React Router'
+        ]
+    },
+    {
+        id: 3,
+        project_image: '.................',
+        project_title: '........',
+        live_url: '............',
+        source_url: '....................',
+        description: '..............',
+        stack: [
+            'React',
+            'Redux',
+            'SASS',
+            'NewsAPI',
+            'Firebase',
+            'React Router',
+            'Axios'
+        ]
+    },
+    {
+        id: 4,
+        project_image: '.............',
+        project_title: '............',
+        live_url: '...........',
+        source_url: '.........',
+        description: '...........',
+        stack: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+        ]
+    },
+    {
+        id: 5,
+        project_image: '...........',
+        project_title: '.......',
+        live_url: '..........',
+        source_url: '...........',
+        description: '.............',
+        stack: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+        ]
+    },
+    {
+        id: 6,
+        project_image: '........',
+        project_title: '.......',
+        live_url: '........',
+        source_url: '.........',
+        description: '.........',
+        stack: [
+            'HTML',
+            'CSS',
+            'JavaScript',
+        ]
+    },
+];
+
+export { projects };
---
+++ src/constants/index.js
@@ -0,0 +1,29 @@
+import AboutSection from "../components/about-section/about-section.component";
+import ContactSection from "../components/contact-section/contact-section.component";
+import ProjectsSection from "../components/projects-section/projects-section.component";
+import SkillsSection from "../components/skills-section/skills-section.component";
+import HeroSection from "../components/hero-section/hero-section.component";
+
+import Navbar from "../components/navbar/navbar.component";
+import Footer from "../components/footer/footer.component";
+
+import SectionTitle from "../components/section-title/section-title.component";
+import ProjectCard from "../components/project-card/project-card.component";
+import Skill from "../components/skill/skill.component";
+import NavigationMenu from "../components/navigation-menu/navigation-menu.component";
+
+export {
+    AboutSection,
+    ContactSection,
+    ProjectsSection,
+    SkillsSection,
+    HeroSection,
+
+    Navbar,
+    Footer,
+
+    SectionTitle,
+    ProjectCard,
+    Skill,
+    NavigationMenu
+};
