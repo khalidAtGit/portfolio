de40018ac417ae3c86453cf8bd0c9941b3bc00e4
khalidAtGit <khalid.work97@gmail.com>
2024-11-15 17:18:06 +0530

    links update

--- src/constants/ProjectsList.js
+++ src/constants/ProjectsList.js
@@ -1,7 +1,7 @@
 const projects = [
     {
         id: 1,
-        project_image: 'https://github.com/khalidAtGit/OchiNew/blob/main/public/assets/Ochi.jpg',
+        project_image: 'https://raw.githubusercontent.com/khalidAtGit/OchiNew/main/public/assets/Ochi.jpg',
         project_title: 'Ochi',
         live_url: 'https://ochi-new-khalidatgits-projects.vercel.app/',
         source_url: 'https://github.com/khalidAtGit/OchiNew',
@@ -16,7 +16,7 @@ const projects = [
     },
     {
         id: 2,
-        project_image: '...........',
+        project_image: 'https://raw.githubusercontent.com/khalidAtGit/OchiNew/main/public/assets/Ochi.jpg',
         project_title: '...........',
         live_url: '........................',
         source_url: '.......................',
@@ -32,7 +32,7 @@ const projects = [
     },
     {
         id: 3,
-        project_image: '.................',
+        project_image: 'https://raw.githubusercontent.com/khalidAtGit/OchiNew/main/public/assets/Ochi.jpg',
         project_title: '........',
         live_url: '............',
         source_url: '....................',
